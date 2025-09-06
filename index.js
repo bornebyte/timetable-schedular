4d3ffd531166ded98fb360fc140825550022a8d0
Shubham Shah <shahshubham1888@gmail.com>
2025-09-06 16:39:12 +0545

    Demo data is ready

---
+++ index.js
@@ -0,0 +1,137 @@
+let classes = [
+    {
+        "name": "011",
+        "department": "CSE"
+    },
+    {
+        "name": "012",
+        "department": "CSE"
+    },
+    {
+        "name": "113",
+        "department": "Computer Science"
+    },
+    {
+        "name": "114",
+        "department": "AI ML"
+    },
+    {
+        "name": "215",
+        "department": "Data Science"
+    },
+    {
+        "name": "216",
+        "department": "Cyber Security"
+    },
+]
+
+
+let teachers = [
+    {
+        "name": "Alok Thakur",
+        "department": "CSE",
+        "subject": "Data Structure"
+    },
+    {
+        "name": "Aman chaurasia",
+        "department": "Cyber Security",
+        "subject": "Math"
+    },
+    {
+        "name": "Raju Chaurasia",
+        "department": "AI ML",
+        "subject": "Physics"
+    },
+    {
+        "name": "Bipin Mandal",
+        "department": "Data Science",
+        "subject": "Chemistry"
+    },
+    {
+        "name": "Laxman Singh",
+        "department": "Computer Science",
+        "subject": "English"
+    },
+    {
+        "name": "Nitesh Kumar",
+        "department": "Cyber Security",
+        "subject": "English"
+    }
+]
+
+let timings = [
+    {
+        "department": "CSE",
+        "times": ["8-45:9-45", "9-45:10-45", "10-45:11-45", "11-45:12-45", "1-45:2-45", "2-45:3-45"]
+    },
+    {
+        "department": "AI ML",
+        "times": ["8-45:9-45", "9-45:10-45", "10-45:11-45", "11-45:12-45", "1-45:2-45", "2-45:3-45"]
+    },
+    {
+        "department": "Data Science",
+        "times": ["8-45:9-45", "9-45:10-45", "10-45:11-45", "11-45:12-45", "1-45:2-45", "2-45:3-45"]
+    },
+    {
+        "department": "Cyber Security",
+        "times": ["8-45:9-45", "9-45:10-45", "10-45:11-45", "11-45:12-45", "1-45:2-45", "2-45:3-45"]
+    },
+    {
+        "department": "Computer Science",
+        "times": ["8-45:9-45", "9-45:10-45", "10-45:11-45", "11-45:12-45", "1-45:2-45", "2-45:3-45"]
+    }
+]
+
+
+let demo_output = [
+    {
+        "class":"011",
+        "teacher":"Alok Thakur",
+        "time":"8-45:9-45"
+    },
+    {
+        "class":"012",
+        "teacher":"Aman chaurasia",
+        "time":"9-45:10-45"
+    },
+    {
+        "class":"113",
+        "teacher":"Raju Chaurasia",
+        "time":"10-45:11-45"
+    },
+    {
+        "class":"114",
+        "teacher":"Bipin Mandal",
+        "time":"11-45:12-45"
+    },
+    {
+        "class":"215",
+        "teacher":"Laxman Singh",
+        "time":"1-45:2-45"
+    },
+    {
+        "class":"216",
+        "teacher":"Nitesh Kumar",
+        "time":"2-45:3-45"
+    },
+    {
+        "class":"011",
+        "teacher":"Alok Thakur",
+        "time":"8-45:9-45"
+    },
+    {
+        "class":"012",
+        "teacher":"Aman chaurasia",
+        "time":"9-45:10-45"
+    },
+    {
+        "class":"113",
+        "teacher":"Raju Chaurasia",
+        "time":"10-45:11-45"
+    },
+    {
+        "class":"114",
+        "teacher":"Bipin Mandal",
+        "time":"11-45:12-45"
+    }
+]
